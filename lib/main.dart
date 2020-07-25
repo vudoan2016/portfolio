5b2014308ae2467c6e401ac5127ee01f51fa346b
vudoan2016 <vudoant@gmail.com>
2020-07-25 01:16:28 -0700

    Added PageView to handle "Summary," "Investment" and "Retirement" pages.

--- lib/main.dart
+++ lib/main.dart
@@ -54,17 +54,29 @@ class MyApp extends StatefulWidget {
   _MyAppState createState() => _MyAppState();
 }
 
+const pageTitles = ['Summary', 'Investment', 'Retirement'];
+
 class _MyAppState extends State<MyApp> {
   Future<List<Asset>> futureAsset;
-  final selectionMax = 5;
-  final winner = 1;
-  final loser = 2;
+  int _index = 0;
+
+  PageController _controller = PageController(
+    initialPage: 0,
+  );
 
   @override
   void initState() {
     super.initState();
     futureAsset = fetchAsset();
   }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _handlePageChange(int index) => setState(() => _index = index);
 
   @override
   Widget build(BuildContext context) {
@@ -74,73 +86,220 @@ class _MyAppState extends State<MyApp> {
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text('Highlight'),
+        appBar: AppBar(centerTitle: true, title: new Text(pageTitles[_index],),
+          actions: <Widget>[
+          IconButton(icon: Icon(Icons.settings, color: Colors.white,),
+            onPressed: () {},
+          )],
         ),
-        body: Center(
-          child: FutureBuilder<List<Asset>>(
-            future: futureAsset,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                DataRow _createDataRow(asset, performer) {
+        body: PageView(
+          controller: _controller,
+          children: [
+            SummaryPage(futureAsset: futureAsset),
+            InvestmentPage(futureAsset: futureAsset),
+            RetirementPage(futureAsset: futureAsset),
+          ],
+          onPageChanged: _handlePageChange,
+        ),
+      ),
+    );
+  }
+}
+
+class SummaryPage extends StatelessWidget {
+  final Future<List<Asset>> futureAsset;
+  final selectionMax = 5;
+  final winner = 1;
+  final loser = 2;
+
+  SummaryPage({Key key, @required this.futureAsset}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FutureBuilder<List<Asset>>(
+        future: futureAsset,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            DataRow _createDataRow(asset, performer) {
                   var color = performer == winner ? Colors.green : Colors.red;
                   return DataRow(
-                    cells: <DataCell>[DataCell(Text(asset.symbol)),
-                      DataCell(Text(asset.price.toStringAsFixed(2))),
-                      DataCell(Text(asset.gain.toStringAsFixed(2), style: TextStyle(color: color),),),],
+                    cells: <DataCell>[DataCell(Text(asset.symbol, style: TextStyle(fontWeight: FontWeight.bold))),
+                      DataCell(Text(asset.price.toStringAsFixed(2), style: TextStyle(fontWeight: FontWeight.bold))),
+                      DataCell(Text(asset.gain.toStringAsFixed(2), style: TextStyle(color: color, fontWeight: FontWeight.bold)),),],
                   );
                 }
-                List<DataRow> _select(data, performer) {
-                  // assets can have the same symbol so assetMap is used to eliminate duplicates
-                  var assetMap = new Map();
-                  var l = List<DataRow>();
-                  var count = 0;
-                  Iterable it = performer == winner ? data : data.reversed;
-                  for (var e in it) {
-                    if (performer == winner && e.gain <= 0 || performer == loser && e.gain >= 0 || count == selectionMax) {
-                      break;
-                    } else if (!assetMap.containsKey(e.symbol) && (e.type == "taxed" || e.type == "deferred")) {
-                      l.add(_createDataRow(e, performer));
-                      assetMap[e.symbol] = true;
-                      count++;
-                    }
-                  }
-                  // List.generate(snapshot.data.length, (index) => _createDataRow(snapshot.data[index])),
-                  return l;
+            List<DataRow> _select(data, performer) {
+              // assets can have the same symbol so assetMap is used to eliminate duplicates
+              var assetMap = new Map();
+              var l = List<DataRow>();
+              var count = 0;
+              Iterable it = performer == winner ? data : data.reversed;
+              for (var e in it) {
+                if (performer == winner && e.gain <= 0 || performer == loser && e.gain >= 0 || count == selectionMax) {
+                  break;
+                } else if (!assetMap.containsKey(e.symbol) && (e.type == "taxed" || e.type == "deferred")) {
+                  l.add(_createDataRow(e, performer));
+                  assetMap[e.symbol] = true;
+                  count++;
                 }
-                DataTable _createDataTable(data, performer) {
-                  return DataTable(
-                    columns: const <DataColumn>[
-                      DataColumn(label: Text('Symbol', style: TextStyle(fontSize: 15,
-                          fontWeight: FontWeight.bold),),),
-                      DataColumn(label: Text('Price', style: TextStyle(fontSize: 15,
-                          fontWeight: FontWeight.bold),),),
-                      DataColumn(label: Text('\u0394(%)', style: TextStyle(fontSize: 15,
-                          fontWeight: FontWeight.bold),),),
-                    ],
-                    rows: _select(data, performer),
-                  );
-                }
-                return Container(
-                  child: ListView(scrollDirection: Axis.vertical, children: <Widget>[
-                    Center(child: Text('Top Gainers', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold))),
-                    SingleChildScrollView(scrollDirection: Axis.vertical, child: _createDataTable(snapshot.data, winner)),
-                    Padding(padding: EdgeInsets.all(20.00)),
-                    Center(child: Text('Top Losers', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold))),
-                    SingleChildScrollView(scrollDirection: Axis.vertical, child: _createDataTable(snapshot.data, loser)),
-                  ],),
-                  alignment: Alignment(-1.0, -1.0),
-                );
-              } else if (snapshot.hasError) {
-                return Text("${snapshot.error}");
               }
+              // List.generate(snapshot.data.length, (index) => _createDataRow(snapshot.data[index])),
+              return l;
+            }
+            DataTable _createDataTable(data, performer) {
+              return DataTable(
+                columns: const <DataColumn>[
+                  DataColumn(label: Text('Symbol', style: TextStyle(fontSize: 15,
+                      fontWeight: FontWeight.bold),),),
+                  DataColumn(label: Text('Price', style: TextStyle(fontSize: 15,
+                      fontWeight: FontWeight.bold),),),
+                  DataColumn(label: Text('\u0394(%)', style: TextStyle(fontSize: 15,
+                      fontWeight: FontWeight.bold),),),
+                ],
+                rows: _select(data, performer),
+              );
+            }
+            return Container(
+              child: ListView(scrollDirection: Axis.vertical, children: <Widget>[
+                Center(child: Text('Top Gainers', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold))),
+                SingleChildScrollView(scrollDirection: Axis.vertical, child: _createDataTable(snapshot.data, winner)),
+                Padding(padding: EdgeInsets.all(20.00)),
+                Center(child: Text('Top Losers', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold))),
+                SingleChildScrollView(scrollDirection: Axis.vertical, child: _createDataTable(snapshot.data, loser)),
+              ],),
+              alignment: Alignment(-1.0, -1.0),
+            );
+          } else if (snapshot.hasError) {
+            return Text("${snapshot.error}");
+          }
 
-              // By default, show a loading spinner.
-              return CircularProgressIndicator();
-            },
-          ),
-        ),
+          // By default, show a loading spinner.
+          return CircularProgressIndicator();
+          },
+      ),
+    );
+  }
+}
+
+class InvestmentPage extends StatelessWidget {
+  final Future<List<Asset>> futureAsset;
+
+  InvestmentPage({Key key, @required this.futureAsset}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FutureBuilder<List<Asset>>(
+        future: futureAsset,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            DataRow _createDataRow(asset) {
+              var color = asset.gain >= 0 ? Colors.green : Colors.red;
+              return DataRow(
+                cells: <DataCell>[DataCell(Text(asset.symbol, style: TextStyle(fontWeight: FontWeight.bold))),
+                  DataCell(Text(asset.price.toStringAsFixed(2), style: TextStyle(fontWeight: FontWeight.bold))),
+                  DataCell(Text(asset.gain.toStringAsFixed(2), style: TextStyle(color: color, fontWeight: FontWeight.bold)),),],
+              );
+            }
+            List<DataRow> _select(data) {
+              var l = List<DataRow>();
+              for (var e in data) {
+                if (e.type == "taxed" && e.symbol != 'ETRADE' && e.symbol != 'MERRILL' && e.symbol != 'VANGUARD') {
+                  l.add(_createDataRow(e));
+                }
+              }
+              // List.generate(snapshot.data.length, (index) => _createDataRow(snapshot.data[index])),
+              return l;
+            }
+            DataTable _createDataTable(data) {
+              return DataTable(
+                columns: const <DataColumn>[
+                  DataColumn(label: Text('Symbol', style: TextStyle(fontSize: 15,
+                      fontWeight: FontWeight.bold),),),
+                  DataColumn(label: Text('Price', style: TextStyle(fontSize: 15,
+                      fontWeight: FontWeight.bold),),),
+                  DataColumn(label: Text('\u0394(%)', style: TextStyle(fontSize: 15,
+                      fontWeight: FontWeight.bold),),),
+                ],
+                rows: _select(data),
+              );
+            }
+            return Container(
+              child: ListView(scrollDirection: Axis.vertical, children: <Widget>[
+                SingleChildScrollView(scrollDirection: Axis.vertical, child: _createDataTable(snapshot.data)),
+              ],),
+              alignment: Alignment(-1.0, -1.0),
+            );
+          }
+          else if (snapshot.hasError) {
+            return Text("${snapshot.error}");
+          }
+
+          // By default, show a loading spinner.
+          return CircularProgressIndicator();
+        },
+      ),
+    );
+  }
+}
+
+class RetirementPage extends StatelessWidget {
+  final Future<List<Asset>> futureAsset;
+
+  RetirementPage({Key key, @required this.futureAsset}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FutureBuilder<List<Asset>>(
+        future: futureAsset,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            DataRow _createDataRow(asset) {
+              var color = asset.gain >= 0 ? Colors.green : Colors.red;
+              return DataRow(
+                cells: <DataCell>[DataCell(Text(asset.symbol, style: TextStyle(fontWeight: FontWeight.bold))),
+                  DataCell(Text(asset.price.toStringAsFixed(2), style: TextStyle(fontWeight: FontWeight.bold))),
+                  DataCell(Text(asset.gain.toStringAsFixed(2), style: TextStyle(color: color, fontWeight: FontWeight.bold)),),],
+              );
+            }
+            List<DataRow> _select(data) {
+              var l = List<DataRow>();
+              for (var e in data) {
+                if (e.type == "deferred" && e.symbol != 'ETRADE' && e.symbol != 'FIDELITY' && e.symbol != 'PAYFLEX') {
+                  l.add(_createDataRow(e));
+                }
+              }
+              // List.generate(snapshot.data.length, (index) => _createDataRow(snapshot.data[index])),
+              return l;
+            }
+            DataTable _createDataTable(data) {
+              return DataTable(
+                columns: const <DataColumn>[
+                  DataColumn(label: Text('Symbol', style: TextStyle(fontSize: 15,
+                      fontWeight: FontWeight.bold),),),
+                  DataColumn(label: Text('Price', style: TextStyle(fontSize: 15,
+                      fontWeight: FontWeight.bold),),),
+                  DataColumn(label: Text('\u0394(%)', style: TextStyle(fontSize: 15,
+                      fontWeight: FontWeight.bold),),),
+                ],
+                rows: _select(data),
+              );
+            }
+            return Container(
+              child: ListView(scrollDirection: Axis.vertical, children: <Widget>[
+                SingleChildScrollView(scrollDirection: Axis.vertical, child: _createDataTable(snapshot.data)),
+              ],),
+              alignment: Alignment(-1.0, -1.0),
+            );
+          } else if (snapshot.hasError) {
+            return Text("${snapshot.error}");
+          }
+
+          // By default, show a loading spinner.
+          return CircularProgressIndicator();
+        },
       ),
     );
   }
